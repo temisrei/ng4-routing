9e0829b3e5ad41996b6071b4d16a285fb7f6a7a7
Rei Lin <temisrei@gmail.com>
2017-08-22 22:47:50 +0800

    修正路由在  UserComponent 的設定

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { ServersService } from './servers/servers.service';
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'users', component: UsersComponent },
-  { path: 'users/:id/:name', component: UsersComponent },
+  { path: 'users/:id/:name', component: UserComponent },
   { path: 'servers', component: ServersComponent },
   { path: 'servers/:id', component: ServerComponent },
   { path: 'servers/:id/edit', component: EditServerComponent }
