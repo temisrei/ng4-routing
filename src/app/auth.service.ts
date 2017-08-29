a94b6f362fe891194460cc2d919ec465c0006738
Rei Lin <temisrei@gmail.com>
2017-08-29 23:05:55 +0800

    ng g s auth --spec=false 暫時的 Fake AuthService

---
+++ src/app/auth.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthService {
+  loggedIn = false;
+
+  constructor() { }
+
+  login() {
+    this.loggedIn = true;
+  }
+
+  logout() {
+    this.loggedIn = false;
+  }
+
+  isAuthenticated() {
+    const promise = new Promise(
+      (resolve, reject) => {
+        setTimeout(() => {
+          resolve(this.loggedIn)
+        }, 800);
+      }
+    );
+  }
+
+}
