769bdde12e2be9bf47e7b89b4b47bd40cb346674
Rei Lin <temisrei@gmail.com>
2017-08-29 23:10:15 +0800

    實作 canActivate() 方法

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private authSvc: AuthService) { }
 
   canActivate(route: ActivatedRouteSnapshot,
               state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-                return false;
+                // return false;
+                this.authSvc.isAuthenticated()
+                  .then(
+                    (authenticated: boolean) => {
+                      if (authenticated) {
+                        return true;
+                      } else {
+                        return false;
+                      }
+                    }
+                  );
               }
 }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -22,6 +22,7 @@ export class AuthService {
         }, 800);
       }
     );
+    return promise;
   }
 
 }
