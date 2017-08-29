2e1464770bc9173850d6446b046105fbcd079244
Rei Lin <temisrei@gmail.com>
2017-08-29 23:00:42 +0800

    設定 Injectable 裝飾器在類別上並實作 CanActivate 介面

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
   constructor() { }
 
+  canActivate(route: ActivatedRouteSnapshot,
+              state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+                return false;
+              }
 }
