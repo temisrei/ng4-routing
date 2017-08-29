bfdd3a7029853639ef3791eaf75566697c58e7d4
Rei Lin <temisrei@gmail.com>
2017-08-29 23:13:26 +0800

    重新確認 canActivate 應回傳 boolean

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,22 +1,24 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from './auth.service';
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
 
-  constructor(private authSvc: AuthService) { }
+  constructor(private authSvc: AuthService,
+              private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot,
               state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
                 // return false;
-                this.authSvc.isAuthenticated()
+                return this.authSvc.isAuthenticated()
                   .then(
                     (authenticated: boolean) => {
                       if (authenticated) {
                         return true;
                       } else {
+                        this.router.navigate(['/']);
                         return false;
                       }
                     }
