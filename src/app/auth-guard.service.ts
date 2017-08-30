9d61ab2482a8e98c307516365020308afe2bb744
Rei Lin <temisrei@gmail.com>
2017-08-30 23:09:39 +0800

    實作 canActivateChild 介面 實作 canActivateChild 方法

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,27 +1,34 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import {CanActivateChild, CanActivate,  ActivatedRouteSnapshot,  RouterStateSnapshot,  Router} from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from './auth.service';
 
 @Injectable()
-export class AuthGuardService implements CanActivate {
+export class AuthGuardService implements CanActivate, CanActivateChild {
 
   constructor(private authSvc: AuthService,
               private router: Router) { }
 
-  canActivate(route: ActivatedRouteSnapshot,
-              state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-                // return false;
-                return this.authSvc.isAuthenticated()
-                  .then(
-                    (authenticated: boolean) => {
-                      if (authenticated) {
-                        return true;
-                      } else {
-                        this.router.navigate(['/']);
-                        return false;
-                      }
-                    }
-                  );
-              }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+      // return false;
+      return this.authSvc.isAuthenticated()
+        .then(
+          (authenticated: boolean) => {
+            if (authenticated) {
+              return true;
+            } else {
+              this.router.navigate(['/']);
+              return false;
+            }
+          }
+        );
+    }
+
+  canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+      return this.canActivate(route, state);
+    }
 }
